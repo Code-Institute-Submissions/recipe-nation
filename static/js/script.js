f0e775402452447acdf429301e4704de5515341e
hollyford <holly_ford@hotmail.com>
2021-01-20 20:01:34 +0000

    Added styling to header, added footer, added carousel content

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $(".dropdown-trigger").dropdown({ hover: true });
+    $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+  });
