44ff4e59c47f29b22c403a3ebe6b491396af6966
hollyford <holly_ford@hotmail.com>
2021-01-21 10:16:03 +0000

    amended dropdown to show below trigger. Added caret font awesome pic

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
-    $(".dropdown-trigger").dropdown({ hover: true });
+    $(".dropdown-trigger").dropdown({ 
+        hover: true,
+        coverTrigger: false
+     });
     $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true
